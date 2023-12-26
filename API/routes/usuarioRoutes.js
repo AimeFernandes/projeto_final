0641fbb3adb2a7fcd60be6d7919e19676539f27d
Iran-Ferreira <97695897+Iran-Ferreira@users.noreply.github.com>
2023-12-26 10:31:03 -0300

    feat: user

--- API/routes/usuarioRoutes.js
+++ API/routes/usuarioRoutes.js
@@ -4,12 +4,6 @@ const usuarioController = require('../controllers/usuarioController.js');
 const routes = new Router();
 
 routes
-  .get("/listarProdutos", usuarioController.listarProdutos)
-  .get("/buscarProduto/:id", usuarioController.buscarProduto)
-  .get("/buscarProdutoNome/:nome", usuarioController.buscarProdutoNome)
   .post("/cadastro", usuarioController.cadastrar)
-  .post("/produtos", usuarioController.addProduto) 
-  .put("/editarProduto", usuarioController.editarProduto)
-  .delete("/deletarProduto/:id", usuarioController.deletarProduto);
 
 module.exports = routes;
