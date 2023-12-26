a862c1a693ee59de6e81037f42e2141e6c9c79b9
Iran-Ferreira <97695897+Iran-Ferreira@users.noreply.github.com>
2023-12-26 10:30:43 -0300

    build

--- API/database/db_connection.js
+++ API/database/db_connection.js
@@ -1,8 +1,14 @@
 const pg = require('pg');
+const dotenv = require("dotenv")
+dotenv.config()
 
 async function conectar() {
   const pool = new pg.Pool({  //Conexão com o banco de dados postgres
-    connectionString: `postgres://aluno_20201214010001:142783@177.136.201.182:5439/temp?schema=aluno_20201214010001`,
+    user: process.env.PGUSER,
+    host: process.env.PGHOST,
+    database: process.env.PGDATABASE,
+    password: process.env.PGPASSWORD,
+    port: process.env.PGPORT
   });
   const conexaoBancoDeDados = await pool.connect();
   console.log("Banco de dados conectado com sucesso!");
--- app.js
+++ app.js
@@ -2,10 +2,11 @@ const express = require('express');
 const cors = require('cors');
 const usuario = require('./API/routes/usuarioRoutes.js');
 const login = require('./API/routes/login.js');
+const produto = require("./API/routes/produtoRoutes")
 
 const app = express();
 
-app.use(cors("*"), express.json(), login, usuario);
+app.use(cors("*"), express.json(), login, usuario, produto);
 
 app.listen(3000, () => {
   console.log("A API está rodando na porta 3000");
