3a19d9b27c2283508abb7a25890c6e1a992cf2cd
Iran-Ferreira <97695897+Iran-Ferreira@users.noreply.github.com>
2023-12-28 16:26:48 -0300

    fix

--- sistema/main.js
+++ sistema/main.js
@@ -1,76 +1,87 @@
 const baseUrl = "http://localhost:3000"
-    
-    getDados();
+
+getDados();
 
 async function addPeca() {
-    const inputNome = document.querySelector("#inputNome")
-    const inputPreco = document.querySelector("#inputPreco");
-    const inputDescricao = document.querySelector("#inputDescricao");
-    const inputQuantidade = document.querySelector("#inputQuantidade");
-    const dados = {
-        nome: inputNome.value,
-        preco: inputPreco.value,
-        descricao: inputDescricao.value,
-        quantidade: inputQuantidade.value
-    }
 
-    try {
-        await fetch(baseUrl + "/addPeca", {
-        method: "POST",
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify(dados)
-        });
-        location.reload();
-    } catch (error) {
-        alert("Ocorreu um erro" + error)
-    }
+    const cadastro = document.querySelector("#cadastro")
+    cadastro.addEventListener("click", async() =>{
 
-    //Limpar dados 
-    inputNome.value = "";
-    inputPreco.value = "";
-    inputDescricao.value = "";
-    inputQuantidade.value = "";
+        const inputNome = document.querySelector("#inputNome").value
+        const inputPreco = document.querySelector("#inputPreco").value
+        const inputDescricao = document.querySelector("#inputDescricao").value
+        const inputQuantidade = document.querySelector("#inputQuantidade").value
 
-    //Fechar janela
-    const m = document.querySelector("#exampleModal");
-    const modal = bootstrap.Modal.getInstance(m);
-    modal.hide();
-    exibirDados();
+        const dados = {
+            nome: inputNome,
+            quantidade: inputQuantidade,
+            preco: inputPreco,
+            descricao: inputDescricao
+        }
+
+        console.log("Dados: ", dados)
+
+        try {
+            await fetch(baseUrl + "/addPeca", {
+            method: "POST",
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(dados)
+            });
+            location.reload();
+        } catch (error) {
+            alert("Ocorreu um erro" + error)
+        }
+
+        //Limpar dados 
+        inputNome.value = "";
+        inputPreco.value = "";
+        inputDescricao.value = "";
+        inputQuantidade.value = "";
+
+        //Fechar janela
+        const m = document.querySelector("#exampleModal");
+        const modal = bootstrap.Modal.getInstance(m);
+        modal.hide();
+        exibirDados();
+    })
+
 }
 
-async function exibirDados(agenda) {
+async function exibirDados(data) {
 
     const tbody = document.querySelector("#dadosEstoque");
+    console.log(data)
 
     let linhas = "";
-    agenda.forEach((dado, index) => {
-        linhas += `<tr>
-            <td>${index + 1}</td>
-            <td>${dado.nome}</td>
-            <td>${dado.preco}</td>
-            <td>${dado.descricao}</td>
-            <td>${dado.quantidade}</td>
-            <td><i class="bi bi-pencil-square" style="cursor: pointer" data-bs-toggle="modal" data-bs-target="#editModal" onclick="preencherCampos('${dado.id}')"></i></td>
-            <td><i class="bi bi-trash" style="cursor: pointer" onclick="removerDados(${dado.id})"></i></td>
-        </tr>`;
-    });
 
-    if (linhas == "") {
+    if(!data || data.length == 0){
         linhas = `
-        <tr>
-            <td colspan="5" style="text-align:center">Não há dados cadastrados</td>
-        </tr>
-        `;
+            <tr>
+                <td colspan="5" style="text-align:center"> Não há peças cadastradas</td>
+            </tr>
+        `
+    }else {
+        data.forEach((dado, index) => {
+            linhas += `<tr>
+                <td>${index + 1}</td>
+                <td>${dado.nome}</td>
+                <td>${dado.preco}</td>
+                <td>${dado.descricao}</td>
+                <td>${dado.quantidade}</td>
+                <td><i class="bi bi-pencil-square" style="cursor: pointer" data-bs-toggle="modal" data-bs-target="#editModal" onclick="preencherCampos('${dado.id}')"></i></td>
+                <td><i class="bi bi-trash" style="cursor: pointer" onclick="removerDados(${dado.id})"></i></td>
+            </tr>`;
+        });
     }
 
     tbody.innerHTML = linhas;
-
 }
 
 async function getDados(){
-
     try {
-        const response = await fetch(baseUrl + "/listarPecas");
+        const response = await fetch(baseUrl + "/listarPecas", {
+            method: "GET"
+        });
         const data = await response.json();
         exibirDados(data);
     } catch (error) {
@@ -107,7 +118,7 @@ async function removerDados(id){
         try {
             await fetch(baseUrl + "/deletarPeca/" + id, { method: "DELETE" });
             modalRemover.hide();
-            exibirDados(agenda);
+            exibirDados(data);
         } catch (error) {
             document.location.reload();
         }
